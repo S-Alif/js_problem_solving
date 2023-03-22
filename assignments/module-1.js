b9edeb3360b7dc06be872eb7980588657e8cbf3d
Alif <79266506+S-Alif@users.noreply.github.com>
2023-03-22 19:46:57 +0600

    js exercise

---
+++ assignments/module-1.js
@@ -0,0 +1,9 @@
+/* Write a program that generates a multiplication table for a given number using a for loop. */
+
+function multiplicationTable(num){
+    for(var i = 1; i <= 10; i++){
+        console.log(""+num+" X "+i+" = "+num*i);
+    }
+}
+
+multiplicationTable(9);
